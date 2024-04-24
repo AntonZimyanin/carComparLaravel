63c8af2b15d04660719f900c2106177319784044
AntonZimyanin <zimyanin88@gmail.com>
2024-04-24 16:05:55 +0300

    Add Bot model

---
+++ app/Models/Bot.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use DefStudio\Telegraph\Models\TelegraphBot as BaseModel;
+
+class Bot extends BaseModel
+{
+    const NEUTRAL_STATE = 0;
+    const PRICE_STATE = 1;
+}
